fbf7a482cc4f44054dab431f54c0dd38c59fc307
mc222sn <mc222sn@student.lnu.se>
2018-09-23 13:11:16 +0200

    Added function median

--- src/statistics.js
+++ src/statistics.js
@@ -95,11 +95,52 @@ function mean(numbers) {
 }
 
 
+/**
+ * Returns the median value of the array.
+ *
+ * @param {number[]} numbers
+ * @throws {TypeError} The passed argument is not an array.
+ * @throws {Error} The passed array contains no elements.
+ * @throws {TypeError} The passed array contains not just numbers.
+ * @returns {number}
+ */
+function median(numbers) {
+    if (!Array.isArray(numbers)) {
+        throw new TypeError('The passed argument is not an array.')
+    } else if (numbers.length < 1) {
+        throw new Error('The passed array contains no elements.')
+    } else if (containsOtherDataTypes(numbers)) {
+        throw new TypeError('The passed array contains not just numbers.')
+    }
+
+    let newArr = [...numbers]
+    newArr.sort((a, b) => a - b)
+
+    if (newArr.length % 2 === 0) {
+        for (let i = 0; i < newArr.length; i++) {
+            if (newArr.length > 2) {
+                newArr.pop()
+                newArr.shift()
+            }
+        }
+        return (newArr[0] + newArr[1]) / 2
+    } else {
+        for (let i = 0; i < newArr.length; i++) {
+            if (newArr.length > 1) {
+                newArr.pop()
+                newArr.shift()
+            }
+        }
+        return newArr[0]
+    }
+}
+
+
 // Exports
 exports.descriptiveStatistics = descriptiveStatistics
 exports.maximum = maximum
 exports.mean = mean
-exports.median = undefined
+exports.median = median
 exports.minimum = undefined
 exports.mode = undefined
 exports.range = undefined
